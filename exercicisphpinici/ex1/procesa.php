47d6338f634b43a7a46e1d2dc74bf1fa0d70df33
polblasco <146941465+polblasco@users.noreply.github.com>
2025-10-24 21:29:27 +0200

    Delete exercicisphpinici directory

--- exercicisphpinici/ex1/procesa.php
+++
@@ -1,5 +0,0 @@
-<?php
-$nom = $_POST['nom'];
-$email = $_POST['email'];
-echo "Missatge rebut, $nom. Gràcies per contactar. Et respondrem a $email.";
-?>
--- exercicisphpinici/ex5/calcular.php
+++
@@ -1,7 +0,0 @@
-<?php
-$preu = $_POST["preu"];
-$iva = $_POST["iva"];
-
-$total = $preu + ($preu * $iva / 100);
-echo "El preu amb IVA es: $total €";
-?>
